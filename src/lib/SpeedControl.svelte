2de319c9da1f6c980f6f5fe439109d683981b976
Wilker Lucio <wilkerlucio@gmail.com>
2024-08-09 09:07:15 -0300

    Speed control via buttons

--- src/lib/SpeedControl.svelte
+++ src/lib/SpeedControl.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+  import Icon from "@/lib/Icon.svelte";
+
   export let video = document.querySelector("video")
+
+  function increment(e) {
+    return e.shiftKey ? 0.01 : 0.1
+  }
 </script>
 
 <div class="container">
   <div class="label" on:click={() => video.playbackRate = 1}>Speed</div>
+  <Icon icon="minus-circle" on:click={(e) => video.playbackRate -= increment(e)} />
   <input type="range" min="0.1" max="2" step="0.01" bind:value={video.playbackRate}/>
+  <Icon icon="plus-circle" on:click={(e) => video.playbackRate += increment(e)} />
   <div>{Math.round(video.playbackRate * 100)}%</div>
 </div>
 
