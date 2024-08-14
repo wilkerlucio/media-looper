2f0442e09eff659871c03002eabe2b06c0b82321
Wilker Lucio <wilkerlucio@gmail.com>
2024-08-14 02:07:57 -0300

    Remove automerge setup code from background

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,26 +1,6 @@
 import {createIndexedDbPersister} from 'tinybase/persisters/persister-indexed-db';
-import {createStore, Store} from 'tinybase';
-import {Repo} from "@automerge/automerge-repo/slim";
 import {setupStore} from "@/lib/stores/core";
 import {backgroundListen, combineSenders} from "@/lib/misc/chrome-network";
-
-async function setupLocalStore() {
-  const store = createStore()
-  const persister = createIndexedDbPersister(store, 'media-looper-background');
-
-  await persister.load()
-  await persister.startAutoSave()
-
-  return store
-}
-
-function ensureAutomergeDocURL(store: Store, repo: Repo) {
-  if (!store.getValue("media-looper-automerge-doc-url")) {
-    const doc = repo.create()
-
-    store.setValue("media-looper-automerge-doc-url", doc.url)
-  }
-}
 
 export default defineBackground({
   persistent: true,
