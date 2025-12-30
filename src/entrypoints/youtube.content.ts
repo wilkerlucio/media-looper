fd7d0c097d9d010d15c68f7601864f75a96e232e
Wilker Lucio <wilkerlucio@gmail.com>
2025-12-30 11:14:58 -0300

    Remove GA

--- src/entrypoints/youtube.content.ts
+++ src/entrypoints/youtube.content.ts
@@ -5,24 +5,24 @@ export default defineContentScript({
   allFrames: true,
   matches: ["*://*.youtube.com/*"],
   main() {
-    const appVersion = browser?.runtime?.getManifest()?.version || "0.0.0-dev"
+    // const appVersion = browser?.runtime?.getManifest()?.version || "0.0.0-dev"
 
     // Initialize Google Analytics
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-D9MN9TZ3L5';
-    document.head.appendChild(script);
-
-    (window as any).dataLayer = (window as any).dataLayer || [];
-    function gtag(...args: any[]) {
-      (window as any).dataLayer.push(args);
-    }
-    (window as any).gtag = gtag;
-
-    gtag('js', new Date());
-    gtag('config', 'G-D9MN9TZ3L5', {
-      app_version: appVersion
-    });
+    // const script = document.createElement('script');
+    // script.async = true;
+    // script.src = 'https://www.googletagmanager.com/gtag/js?id=G-D9MN9TZ3L5';
+    // document.head.appendChild(script);
+    //
+    // (window as any).dataLayer = (window as any).dataLayer || [];
+    // function gtag(...args: any[]) {
+    //   (window as any).dataLayer.push(args);
+    // }
+    // (window as any).gtag = gtag;
+    //
+    // gtag('js', new Date());
+    // gtag('config', 'G-D9MN9TZ3L5', {
+    //   app_version: appVersion
+    // });
 
     mount(Youtube, {
       target: document.body
